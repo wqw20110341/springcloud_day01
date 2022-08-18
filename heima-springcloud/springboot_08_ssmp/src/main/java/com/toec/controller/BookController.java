9d490a41576e8ba3d9e2e6530191c5bd8662ccb2
wqw201103 <1258276740@qq.com>
2022-08-19 00:15:26 +0800

    springboot整合项目的配置详解

--- heima-springcloud/springboot_08_ssmp/src/main/java/com/toec/controller/BookController.java
+++ heima-springcloud/springboot_08_ssmp/src/main/java/com/toec/controller/BookController.java
@@ -1,6 +1,10 @@
 package com.toec.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.toec.utils.ResultInfo;
 import com.toec.domain.Book;
+import com.toec.domain.BookFilter;
 import com.toec.service.impl.IBookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,30 +22,59 @@ public class BookController {
     private IBookServiceImpl iBookService;
 
     @GetMapping
-    public List<Book> getAll(){
-        return iBookService.list();
+    public ResultInfo getAll(){
+        List<Book> list = iBookService.list();
+        ResultInfo resultInfo = new ResultInfo(true, list);
+        return resultInfo;
     }
 
     @PostMapping
-    public Boolean save(@RequestBody  Book book){
-        return iBookService.save(book);
+    public ResultInfo save(@RequestBody  Book book){
+        boolean save = iBookService.save(book);
+        ResultInfo resultInfo = new ResultInfo(save);
+        return resultInfo;
     }
 
     @PutMapping
-    public Boolean update(@RequestBody Book book){
-        return iBookService.updateById(book);
+    public ResultInfo update(@RequestBody Book book){
+        boolean updateById = iBookService.updateById(book);
+        ResultInfo resultInfo = new ResultInfo(updateById);
+        return resultInfo;
     }
 
     @DeleteMapping("{id}")
-    public Boolean delete(@PathVariable Integer id){
-        return iBookService.removeById(id);
+    public ResultInfo delete(@PathVariable Integer id){
+        boolean removeById = iBookService.removeById(id);
+        ResultInfo resultInfo = new ResultInfo(removeById);
+        return resultInfo;
     }
 
     @GetMapping("{id}")
-    public Book getById(@PathVariable Integer id){
-        return iBookService.getById(id);
+    public ResultInfo getById(@PathVariable Integer id){
+        Book byId = iBookService.getById(id);
+        ResultInfo resultInfo = new ResultInfo(true,byId);
+        return resultInfo;
     }
 
+    @PostMapping("/filter")
+    public ResultInfo getPage(@RequestBody BookFilter bookFilter){
+        int curPage = bookFilter.getPage();
+        int pageSize = bookFilter.getPageSize();
+        Page page = new Page(curPage, pageSize);
+        LambdaQueryWrapper<Book> wrapper = new LambdaQueryWrapper<>();
+        String name = bookFilter.getName();
+        String type = bookFilter.getType();
+        String description = bookFilter.getDescription();
+        wrapper.like(name!=null,Book::getName,name);
+        wrapper.like(type!=null,Book::getType,type);
+        wrapper.like(description!=null,Book::getDescription,description);
+        Page pageResult = iBookService.page(page, wrapper);
 
-
+        if(curPage > pageResult.getPages()){
+            page.setCurrent(pageResult.getPages());
+            pageResult = iBookService.page(page, wrapper);
+        }
+        ResultInfo resultInfo = new ResultInfo(true, pageResult);
+        return resultInfo;
+    }
 }
